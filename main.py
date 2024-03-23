75b4bd690a912865b96ee3d6e95d2e5460ffb2f8
Nikita Titarenko <ntv.cnd@gmail.com>
2024-03-23 23:48:54 +0300

    logger

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+from dotenv import load_dotenv
+import os
+
+
+logger = logging.getLogger('kurwa')
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO)
+
+load_dotenv()
+
+TG_TOKEN = os.getenv('TOKEN')
+logger.error('fff')
+print(TG_TOKEN)
